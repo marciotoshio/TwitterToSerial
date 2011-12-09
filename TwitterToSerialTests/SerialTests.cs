76ff04f83b6dcac22faf14424f0e135bd7265458
Marcio Toshio <contato@marciotoshio.com.br>
2011-12-09 19:23:26 -0200

    initial commit

---
+++ TwitterToSerialTests/SerialTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace TwitterToSerialTests
+{
+    [TestFixture]
+    public class SerialTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
